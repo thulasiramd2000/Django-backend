daa44f6b5cd4625f96289fdaf644b911bf4733d3
thulasiramd2000 <77000679+thulasiramd2000@users.noreply.github.com>
2024-07-18 20:23:01 -0700

    Initial commit of backend files

---
+++ base/products.py
@@ -0,0 +1,77 @@
+products = [
+  {
+    '_id': '1',
+    'name': 'Chicken Burger',
+    'image': '/images/Burger.jpg',
+    'description':
+      'A burger is a sandwich featuring a ground meat patty, typically beef, inside a sliced bun, often topped with lettuce, tomato, cheese, and condiments',
+    'category': 'Food',
+    'price': 79.99,
+    'countInStock': 10,
+    'rating': 4.5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '2',
+    'name': 'Donut',
+    'image': '/images/Donut.jpg',
+    'description':
+      'A donut is a sweet, fried dough confection typically shaped like a ring or filled with cream or jelly',
+    'category': 'Food',
+    'price': 599.99,
+    'countInStock': 7,
+    'rating': 4.0,
+    'numReviews': 8,
+  },
+  {
+    '_id': '3',
+    'name': 'Italian Pasta',
+    'image': '/images/Pasta.jpg',
+    'description':
+      'Pasta is an Italian dish made from unleavened dough of wheat flour, water, and sometimes eggs, typically boiled and served with a variety of sauces',
+    'category': 'Food',
+    'price': 929.99,
+    'countInStock': 5,
+    'rating': 3,
+    'numReviews': 12,
+  },
+  {
+    '_id': '4',
+    'name': 'Ramensoup',
+    'image': '/images/Ramen-soup.jpg',
+    'description':
+      'Ramen soup is a Japanese dish consisting of wheat noodles served in a savory broth, often flavored with soy sauce or miso, and topped with ingredients like sliced pork, nori, and green onions.',
+    'category': 'Food',
+    'price': 399.99,
+    'countInStock': 11,
+    'rating': 5,
+    'numReviews': 12,
+  },
+  {
+    '_id': '5',
+    'name': 'Rice',
+    'image': '/images/Rice.jpg',
+    'description':
+      'Rice is a staple grain, typically boiled or steamed, and serves as a base for various dishes worldwide, known for its versatility and mild flavor',
+    'category': 'Electronics',
+    'price': 49.99,
+    'countInStock': 7,
+    'rating': 3.5,
+    'numReviews': 10,
+  },
+  {
+    '_id': '6',
+    'name': 'Steak',
+    'image': '/images/Steak.jpg',
+    'description':
+      'Steak is a thick, juicy slice of beef, typically grilled or pan-seared, often seasoned or marinated, and served as a main course',
+    'category': 'Food',
+    'price': 29.99,
+    'countInStock': 0,
+    'rating': 4,
+    'numReviews': 12,
+  },
+]
+
+
+
